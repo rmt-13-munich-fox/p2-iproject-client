e3c1ec62dac237332256c33d206d48216fac72ae
nwrsadat <anwarsadat.juni@gmail.com>
2021-07-28 15:04:15 +0700

    Login page created

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
