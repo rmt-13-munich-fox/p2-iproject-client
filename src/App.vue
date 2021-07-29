54820966b76526a66c92f84cb825d2e61d05be77
Abdan Zam Zam Ramadhan <rabdanzamzam96@gmail.com>
2021-07-30 06:38:13 +0700

    final

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,21 @@
   <div>
     <Navbar />
     <router-view />
+    <HFooter v-if="lastKeyword.length !== 0" />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import HFooter from "vue-hacktiv8-footer";
+import { mapState } from "vuex";
 
 export default {
   name: "App",
-  components: { Navbar },
+  components: { Navbar, HFooter },
+  computed: {
+    ...mapState(["lastKeyword"]),
+  },
 };
 </script>
 
--- src/apis/server.js
+++ src/apis/server.js
@@ -2,4 +2,6 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://bigsource-search-engine.herokuapp.com",
+  // baseURL: "https://search-engine.bigsourceproject.com",
+  // baseURL: "http://localhost:3000",
 });
