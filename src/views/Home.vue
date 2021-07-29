7ebaae631eba5cacd73a367446a1ab6915951343
Abdan Zam Zam Ramadhan <rabdanzamzam96@gmail.com>
2021-07-29 07:56:33 +0700

    Speech to Text and Corona Viruses

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,38 @@
 <template>
   <div>
-    <result-search
-      v-for="data in searchResult"
-      :key="data.id"
-      :data="data"
-    ></result-search>
+    <section class="text-gray-600 body-font overflow-hidden">
+      <div class="container px-5 py-20 mx-auto">
+        <div class="flex flex-wrap -m-12">
+          <div class="md:w-2/3 flex flex-col items-start pl-60 pr-10">
+            <result-search
+              v-for="data in searchResult"
+              :key="data.id"
+              :data="data"
+            >
+            </result-search>
+          </div>
+          <div class="md:w-1/3 flex flex-col items-start pr-10">
+            <card-corona
+              v-if="coronaViruses.length !== 0 && searchResult.length !== 0"
+              :coronaViruses="coronaViruses"
+            ></card-corona>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 import ResultSearch from "../components/ResultSearch.vue";
+import CardCorona from "../components/CardCorona.vue";
 
 export default {
   name: "Home",
-  components: { ResultSearch },
+  components: { ResultSearch, CardCorona },
   computed: {
-    ...mapState(["searchResult"]),
+    ...mapState(["searchResult", "coronaViruses"]),
   },
 };
 </script>
